0d68f96dd24f5410789abd3079c24ba93d1f51a3
FeJustino <fwxjustino@gmail.com>
2025-07-25 22:05:12 -0300

    feat: implement user registration with validation and hashing

---
+++ src/http/controllers/register.controller.ts
@@ -0,0 +1,34 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import z from 'zod'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (userWithSameEmail) {
+    return reply.code(409).send({ message: 'Email já cadastrado' })
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+
+  return reply.code(201).send({ message: 'Usuario criado com sucesso' })
+}
