6ba8037b457dbd3511f9be2d4affa31e723ded9c
FeJustino <fwxjustino@gmail.com>
2025-07-25 22:34:06 -0300

    feat: refactor user registration to use RegisterUseCase and PrismaUsersRepository

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,4 +1,5 @@
-import { registerUseCase } from '@/usecases/register.usecase'
+import { PrismaUsersRepository } from '@/repositories/prisma-user.repository'
+import { RegisterUseCase } from '@/usecases/register.usecase'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 
@@ -10,9 +11,15 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   })
 
   const { name, email, password } = registerBodySchema.parse(request.body)
+
   try {
-    await registerUseCase({ name, email, password })
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({ name, email, password })
+
   } catch (error) {
+
     return reply.code(409).send({ message: 'Email já cadastrado' })
   }
 
--- src/usecases/register.usecase.ts
+++ src/usecases/register.usecase.ts
@@ -1,5 +1,4 @@
 import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-user.repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -8,26 +7,24 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const password_hash = await hash(password, 6)
+export class RegisterUseCase {
+  constructor(private userRepository: any) {}
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: { email },
-  })
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
 
-  if (userWithSameEmail) {
-    throw new Error('email already exist')
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: { email },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('email already exist')
+    }
+
+    await this.userRepository.create({
+      name,
+      password_hash,
+      email,
+    })
   }
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    name,
-    password_hash,
-    email,
-  })
 }
